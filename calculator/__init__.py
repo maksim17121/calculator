0b799dd6a39fd1da1860b27e82500b2d71898e97
maksim17121 <maksim17121@gmail.com>
2025-12-23 15:27:50 +0300

    Add files via upload

---
+++ calculator/__init__.py
@@ -0,0 +1,7 @@
+from .basic import add, subtract, multiply
+from .advanced import power, sin, cos, tan, square_root
+
+__all__ = [
+    'add', 'subtract', 'multiply',
+    'power', 'sin', 'cos', 'tan','square_root'
+]
